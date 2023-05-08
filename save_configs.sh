0a4b2d5f16120671bf905ce42f56e418467e670e
azyablov <anton0zyablov@gmail.com>
2023-05-08 14:31:13 +0200

    updated configs and delta configs per topology

--- save_configs.sh
+++ save_configs.sh
@@ -1,3 +1,3 @@
-# Starting SSH deamon on all quagga machines
+# Saving running configuration on all quagga and frr machines
 for i in {01,02,03,04,11,12,13,21,22,23,}; do docker exec -it clab-dcn_demo-router$i /vtysh/vtysh -c 'write memory'; done
-docker exec -it clab-dcn_demo-ext_as /vtysh/vtysh -c 'write memory'
+for i in {31,32,33}; do docker exec -it clab-dcn_demo-router$i vtysh -c 'write memory'; done
